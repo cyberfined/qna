0804f4797645d2d6725ec1a64428784b4738ee1c
cyberfined <cyberfined@protonmail.com>
2021-03-03 18:54:08 +0000

    Implement question and answer specs

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Answer, type: :model do
+  describe 'associations' do
+    it { should belong_to(:question) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:body) }
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Question, type: :model do
+  describe 'associations' do
+    it { should have_many(:answers) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+end
