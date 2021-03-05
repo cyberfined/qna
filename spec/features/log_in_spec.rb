8c34ff67d8c44bb8ff460d909998d296bd2f9603
cyberfined <cyberfined@protonmail.com>
2021-03-05 19:06:27 +0000

    Refactor Log in spec

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -5,17 +5,14 @@ RSpec.feature 'User can log in', %q{
 } do
   given(:user) { create(:user) }
 
-  background { visit new_user_session_path }
-
   scenario 'Registered user logs in' do
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+    sign_in(user)
 
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'Unregistered user tries to log in' do
+    visit new_user_session_path
     fill_in 'Email', with: 'unregistered@something.com'
     fill_in 'Password', with: 'password'
     click_on 'Log in'
