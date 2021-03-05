cf63d8e67d9edf96ee95f171747a44e4d6b37290
cyberfined <cyberfined@protonmail.com>
2021-03-05 13:03:31 +0000

    Replace Answer.count with question.answers.count

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AnswersController, type: :controller do
       it 'creates new answer in the database' do
         expect {
           post :create, params: { answer: attributes_for(:answer), question_id: question.id }
-        }.to change { Answer.count }.by(1)
+        }.to change { question.answers.count }.by(1)
       end
 
       it 'redirects to show view' do
