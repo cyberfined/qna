072d4b5fb7c995a4c39685a993c2b21d0df77749
cyberfined <cyberfined@protonmail.com>
2021-03-06 14:05:30 +0000

    Implement QuestionsController testing with authenticated and unauthenticated user

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,24 +1,42 @@
 RSpec.describe QuestionsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
   describe 'POST #create' do
-    context 'with valid attributes' do
-      it 'creates new question in the database' do
-        expect { post :create, params: { question: attributes_for(:question) } }.to change { Question.count }.by(1)
+    context 'when user is authenticated' do
+      before { sign_in create(:user) }
+
+      context 'with valid attributes' do
+        it 'creates new question in the database' do
+          expect {
+            post :create, params: { question: attributes_for(:question) }
+          }.to change { Question.count }.by(1)
+        end
+
+        it 'redirects to show view' do
+          post :create, params: { question: attributes_for(:question) }
+          expect(response).to redirect_to controller.question
+        end
       end
 
-      it 'redirects to show view' do
-        post :create, params: { question: attributes_for(:question) }
-        expect(response).to redirect_to controller.question
+      context 'with invalid attributes' do
+        it "doesn't create invalid question" do
+          expect {
+            post :create, params: { question: attributes_for(:question, :invalid) }
+          }.not_to change { Question.count }
+        end
+
+        it 're-renders new view' do
+          post :create, params: { question: attributes_for(:question, :invalid) }
+          expect(response).to render_template :new
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it "doesn't create invalid question" do
-        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.not_to change { Question.count }
-      end
-
-      it 're-renders new view' do
-        post :create, params: { question: attributes_for(:question, :invalid) }
-        expect(response).to render_template :new
+    context 'when user is unauthenticated' do
+      it 'tries to create new question in the database' do
+        expect {
+            post :create, params: { question: attributes_for(:question) }
+        }.to_not change { Question.count }
       end
     end
   end
