c397fd531977ee13618f101602d5600cc64066f5
cyberfined <cyberfined@protonmail.com>
2021-03-06 12:44:48 +0000

    Implement watching question and answers spec

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,22 @@
+RSpec.feature 'User can watch a question and answers to it', %q{
+  In order to see body of the question and answers to it or to write my own answer
+  As an unauthenticated or authenticated user
+  I'd like to watch the question's page
+} do
+  given!(:question) { create(:question) }
+  given!(:answers) do
+    # TODO: use faker to randomize answers in factory
+    question.answers.create!([{ body: 'answer1' }, { body: 'answer2' }, { body: 'answer3' }])
+  end
+
+  scenario "User watch the question's page" do
+    visit questions_path
+    click_on question.title
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each do |ans|
+      expect(page).to have_content ans.body
+    end
+  end
+end
