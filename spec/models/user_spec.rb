55931ec06a8a03f7d5f693f0a4764db5e56ff067
cyberfined <cyberfined@protonmail.com>
2021-03-09 13:03:13 +0000

    Implement author_of? method spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,27 @@ RSpec.describe User, type: :model do
     it { should have_many(:questions).dependent(:destroy) }
     it { should have_many(:answers).dependent(:destroy) }
   end
+
+  describe 'author_of? method' do
+    let(:user) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answer) { question.answers.create!(attributes_for(:answer, user: user)) }
+
+    it 'should return true because user owns this question' do
+      expect(user.author_of?(question)).to eq true
+    end
+
+    it 'should return true because user owns this answer' do
+      expect(user.author_of?(answer)).to eq true
+    end
+
+    it "should return true because user doesn't own this question" do
+      expect(another_user.author_of?(question)).to eq false
+    end
+
+    it "should return true because user doesn't own this answer" do
+      expect(another_user.author_of?(answer)).to eq false
+    end
+  end
 end
