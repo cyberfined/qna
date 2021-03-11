d46d33fe009664e228c659d05ff4a84656eda2c2
cyberfined <cyberfined@protonmail.com>
2021-03-11 10:53:36 +0000

    Configure  javascript driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # Use firefox to tests with javascript
+  Capybara.javascript_driver = :selenium_headless
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
